a0062d3f22784ffedcad7aa3287739bad87eb8f2
Harish Reddy <harishudayagiri06@gmail.com>
2026-04-02 18:31:24 -0400

    Added Q2 SQL

---
+++ Basics/Q2.sql
@@ -0,0 +1,22 @@
+You are provided with a table named Products containing information about various products, including their names and prices. Write a SQL query to count number of products in each category based on its price into three categories below. Display the output in descending order of no of products.
+
+ 
+
+1- "Low Price" for products with a price less than 100
+2- "Medium Price" for products with a price between 100 and 500 (inclusive)
+3- "High Price" for products with a price greater than 500.
+Tables: Products
++--------------+-------------+
+| COLUMN_NAME  | DATA_TYPE   |
++--------------+-------------+
+| product_id   | int         |
+| product_name | varchar(20) |
+| price        | int         |
++--------------+-------------+
+
+
+select case when price < 100 then 'Low Price'
+            when price between 100 and 500 then 'Medium Price'
+	   else 'High Price' end as category, count(product_id) as no_of_products  from products
+	   group by 1
+	   order by 2 desc
